fb69c0eeb98e0b39a9c3503454a4fa8d64172d64
darkblacks <62890050+darkblacks@users.noreply.github.com>
2026-05-25 18:01:22 -0300

    feat: adiciona dto de atualizacao de coluna

---
+++ src/kanbans/dto/update-column.dto.ts
@@ -0,0 +1,27 @@
+import { IsBoolean, IsNumber, IsOptional, IsString, MaxLength, Min, MinLength } from "class-validator";
+
+export class UpdateColumnDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  key?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  order?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  isInitial?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  isFinal?: boolean;
+}
