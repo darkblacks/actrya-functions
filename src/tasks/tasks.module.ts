1ab4adcd8ac9337c9e6606f4058d424654201b98
darkblacks <62890050+darkblacks@users.noreply.github.com>
2026-05-25 18:02:53 -0300

    feat: conecta tasks ao modulo de kanbans

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { KanbansModule } from "../kanbans/kanbans.module";
 import { TasksController } from "./tasks.controller";
 import { TasksService } from "./tasks.service";
 
 @Module({
+  imports: [KanbansModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
