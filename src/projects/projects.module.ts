e8db2ba76c940d4099371d9e6891278e3bf87421
darkblacks <62890050+darkblacks@users.noreply.github.com>
2026-05-25 18:02:32 -0300

    feat: conecta projects ao modulo de kanbans

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { KanbansModule } from "../kanbans/kanbans.module";
 import { ProjectsController } from "./projects.controller";
 import { ProjectsService } from "./projects.service";
 
 @Module({
+  imports: [KanbansModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
